dae1ef1b190cb6e387dc85f6de9adbec9d54216f
DESKTOP-5JKIEN5\池尻泰基 <taiki1222sp@gmail.com>
2018-09-28 20:05:49 +0900

    Playerに移動を追加

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [Range(0, 100)]
+    public float MoveSpeed;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+
+        bool Jump = false;
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-MoveSpeed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(MoveSpeed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Z) && !Jump)
+        {
+
+        }
+
+        //Vector2 add_Move = Vector2.zero;
+
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    add_Move.x = -MoveSpeed;
+        //}
+        //else if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    add_Move.x = MoveSpeed;
+        //}
+
+    }
 }
